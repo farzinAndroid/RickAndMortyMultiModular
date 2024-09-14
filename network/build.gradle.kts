938842f6ff394e33e2793143ffe6fe12d3884ada
Farzin <farzinallahyari1234@gmail.com>
2024-09-14 12:27:09 +0330

    v11 update gradle

--- network/build.gradle.kts
+++ network/build.gradle.kts
@@ -43,9 +43,5 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
 
 
-    implementation(libs.ktor.client.core)
-    implementation(libs.ktor.client.okhttp)
-    implementation(libs.ktor.client.content.negotiation)
-    implementation(libs.ktor.serialization.kotlinx.json)
-    implementation(libs.ktor.client.logging)
+    implementation(libs.bundles.ktor)
 }
